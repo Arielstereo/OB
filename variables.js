2d165beb80facf7ce8b0560ca58267d4a183b3d1
Ariel Martinez <arielstereo@msn.com>
2022-08-22 11:29:47 -0300

    variables

---
+++ variables.js
@@ -0,0 +1,20 @@
+const nombre = "Ariel";
+console.log(nombre);
+
+const edad = "45";
+console.log(edad);
+
+const desarrollador = true;
+console.log(desarrollador);
+
+const fechaNacimiento = new Date("august 23 1976");
+console.log(fechaNacimiento);
+
+const libroFavorito = {
+  titulo : "El principito",
+  autor : "Antoine de Saint-Exupéry",
+  fecha : 1943,
+  url: "http://bibliotecadigital.ilce.edu.mx/Colecciones/ObrasClasicas/_docs/ElPrincipito.pdf"
+};
+console.log(libroFavorito);
+
