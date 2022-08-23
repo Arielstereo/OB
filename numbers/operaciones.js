9398e6b654d2126919f198825089d3d471328e2f
Ariel Martinez <arielstereo@msn.com>
2022-08-23 15:56:44 -0300

    Numbers

---
+++ numbers/operaciones.js
@@ -0,0 +1,15 @@
+let altura = 1.70;
+
+let alturaCentimetros = parseFloat(altura) * 100;
+console.log(alturaCentimetros);
+
+let alturaMetros = parseInt(altura);
+console.log(alturaMetros);
+
+let alturaRedonArriba = Math.round(altura);
+console.log(alturaRedonArriba);
+
+let peso = 70.8;
+
+let pesoRedonAbajo = Math.floor(peso);
+console.log(pesoRedonAbajo);
